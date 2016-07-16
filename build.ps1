51c3891d5bd27083ab973cd345d05228fadd8a63
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2016-07-16 17:52:29 +0100

    Add basic project structure

---
+++ build.ps1
@@ -0,0 +1,33 @@
+# Move to the project root folder (current script folder)
+ 
+function Get-ScriptDirectory 
+{  
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value  
+    Split-Path $Invocation.MyCommand.Path  
+}
+
+$rootFolder = (Get-Item (Get-ScriptDirectory)).FullName 
+Set-Location $rootFolder
+
+# Clone the Okra Build system (or use local copy if in parent folder)
+
+If (Test-Path "../Okra-Build")
+{
+    $okraBuildTools = "../Okra-Build"
+}
+Else
+{
+    If (Test-Path "./.build")
+    {
+        Remove-Item "./.build" -Recurse -Force
+    }
+
+    git clone https://github.com/OkraFramework/Okra-Build.git .build
+    $okraBuildTools = "./.build"
+}
+
+# Run the build script
+
+$buildScript = Join-Path $okraBuildTools "build.ps1"
+Invoke-Expression "& `"$buildScript`" -useMSBuild=true $Args"
+exit $LASTEXITCODE
